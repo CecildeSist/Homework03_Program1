cca41892da7f92b448add60f3405f116d3ae4fd6
CecildeSist <arcetwo.com@gmail.com>
2024-10-06 11:13:22 -0400

    added last name edit text

--- app/src/main/java/com/example/homework03_program1/AddNewStudent.java
+++ app/src/main/java/com/example/homework03_program1/AddNewStudent.java
@@ -6,21 +6,20 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class AddNewStudent extends AppCompatActivity {
 
-    TextView txt_j_addStudent_label;
-    EditText et_j_addStudent_firstName;
+    TextView txt_j_add_label;
+    EditText et_j_add_first;
+    EditText et_j_add_last;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_new_student);
         //Connect used GUI elements to code
-        txt_j_addStudent_label = findViewById(R.id.txt_v_Add_Label);
-        et_j_addStudent_firstName = findViewById(R.id.et_v_add_firstName);
+        txt_j_add_label = findViewById(R.id.txt_v_Add_Label);
+        et_j_add_first = findViewById(R.id.et_v_add_firstName);
+        et_j_add_last = findViewById(R.id.et_v_add_lastName);
     }
 }
